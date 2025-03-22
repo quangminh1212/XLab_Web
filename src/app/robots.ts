f3ef175b8ca5e06ca80e664a737c0cd425d687ca
Bach Minh Quang <0947556012abc@gmail.com>
2025-03-23 01:31:58 +0700

    Add Analytics component to layout for enhanced tracking

---
+++ src/app/robots.ts
@@ -0,0 +1,20 @@
+import { MetadataRoute } from 'next';
+
+export default function robots(): MetadataRoute.Robots {
+  return {
+    rules: {
+      userAgent: '*',
+      allow: '/',
+      disallow: [
+        '/api/',
+        '/admin/',
+        '/account/',
+        '/checkout/',
+        '/cart/',
+        '/.well-known/'
+      ],
+    },
+    sitemap: 'https://xlab.com/sitemap.xml',
+    host: 'https://xlab.com',
+  };
+} 
