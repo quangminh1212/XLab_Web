32de980db24659b2e995add658404f9da7d51e33
quangbm <quangbm@vinfast.vn>
2025-03-26 16:02:12 +0700

    Update next.config.js to remove swcMinify, modify header configurations for improved security, and adjust source path for headers.

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost', 'xlab.vn'],
     formats: ['image/webp', 'image/avif'],
@@ -48,19 +47,19 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: '/(.*)',
+        source: '/:path*',
         headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
           {
             key: 'X-Content-Type-Options',
             value: 'nosniff',
           },
           {
-            key: 'X-Frame-Options',
-            value: 'SAMEORIGIN',
-          },
-          {
-            key: 'X-XSS-Protection',
-            value: '1; mode=block',
+            key: 'Referrer-Policy',
+            value: 'strict-origin-when-cross-origin',
           },
         ],
       },
