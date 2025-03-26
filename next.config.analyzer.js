63a64d10a6918d46ffe724d096db44a81171f463
Bach Minh Quang <0947556012abc@gmail.com>
2025-03-26 20:49:36 +0700

    Clear projectproject

--- next.config.analyzer.js
+++
@@ -1,8 +0,0 @@
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
-});
-
-/** @type {import('next').NextConfig} */
-const nextConfig = require('./next.config.js');
-
-module.exports = withBundleAnalyzer(nextConfig); 
