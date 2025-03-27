58a8eb8c9e16e6d5920bc24397e069d6aa55a559
Bach Minh Quang <0947556012abc@gmail.com>
2025-03-27 22:48:53 +0700

    refactor: add "My Products" link to account navigation for enhanced user access

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -152,6 +152,12 @@ export default function AccountPage() {
                     </svg>
                     Cài đặt tài khoản
                   </a>
+                  <a href="#my-products" className="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-50 rounded-md">
+                    <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5 mr-3" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M20 7l-8-4-8 4m16 0l-8 4m8-4v10l-8 4m0-10L4 7m8 4v10M4 7v10l8 4" />
+                    </svg>
+                    Sản phẩm của tôi
+                  </a>
                 </nav>
               </div>
               
