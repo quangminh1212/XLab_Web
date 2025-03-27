18ba4f29ccd4d3dab94295d8ad9eb66706f7cfa0
Bach Minh Quang <0947556012abc@gmail.com>
2025-03-27 18:07:40 +0700

    refactor: simplify next.config.js by removing unnecessary comments and configurations

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  // swcMinify: true, // Removed as it's causing warnings
   poweredByHeader: false,
   images: {
     remotePatterns: [
@@ -16,55 +15,11 @@ const nextConfig = {
     imageSizes: [16, 32, 48, 64, 96, 128, 256],
   },
   
-  // Cấu hình tối giản
-  experimental: {},
-
   // Tắt source maps trong production
   productionBrowserSourceMaps: false,
-
-  // Cấu hình webpack đơn giản với điều chỉnh để tránh lỗi
-  webpack: (config, { dev, isServer }) => {
-    if (dev) {
-      config.optimization = {
-        ...config.optimization,
-        minimize: false,
-        minimizer: [],
-      };
-    }
-
-    // Giải quyết lỗi "Cannot read properties of undefined (reading 'call')"
-    // Tham khảo: https://github.com/webpack/webpack/issues/15582
-    if (!isServer) {
-      // Tắt chức năng splitChunks để tránh lỗi
-      config.optimization.splitChunks = {
-        cacheGroups: {
-          default: false,
-        }
-      };
-      
-      // Đảm bảo hành vi nhất quán cho webpack
-      config.output = {
-        ...config.output,
-        chunkLoadingGlobal: `webpackChunk_${require('./package.json').name}`,
-      };
-
-      // Thêm cấu hình để tránh prefetch và lỗi liên quan
-      config.output.chunkLoading = 'jsonp';
-      
-      // Vô hiệu hóa các tối ưu hóa có thể gây ra lỗi
-      config.optimization.concatenateModules = false;
-      config.optimization.providedExports = false;
-      config.optimization.usedExports = false;
-    }
-    
-    return config;
-  },
   
   // Cấu hình cơ bản
   staticPageGenerationTimeout: 120,
-  
-  // Thư mục build
-  distDir: '.next',
 };
 
-module.exports = nextConfig; 
+module.exports = nextConfig;
