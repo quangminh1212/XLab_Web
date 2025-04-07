c85928a240532dbd806646a1f0fbafa2d9d20c1d
Bach Minh Quang <0947556012abc@gmail.com>
2025-04-07 17:42:14 +0700

    refactor: enhance Header and Home components with improved layout and language support

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+
+interface LayoutProps {
+  children: React.ReactNode
+}
+
+export default function Layout({ children }: LayoutProps) {
+  return (
+    <>
+      <Header />
+      <main className="min-h-screen bg-gradient-to-b from-gray-50 to-white">
+        {children}
+      </main>
+      <Footer />
+    </>
+  )
+} 
