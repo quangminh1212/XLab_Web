0d135daa95306cf0d80fce29cd5939b23a1cb2b9
Bach Minh Quang <0947556012abc@gmail.com>
2025-04-13 00:37:50 +0700

    fix: optimize Google authentication flow and update SessionProvider

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -3,5 +3,9 @@
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 
 export default function SessionProvider({ children }: { children: React.ReactNode }) {
-  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
+  return (
+    <NextAuthSessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
+      {children}
+    </NextAuthSessionProvider>
+  );
 } 
