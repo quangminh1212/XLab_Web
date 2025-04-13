e8854268b8e5616d4b90295e096189094985133d
Bach Minh Quang <0947556012abc@gmail.com>
2025-04-13 13:48:58 +0700

    refactor: remove unused environment files and clean up authentication code

--- src/components/SessionProvider.tsx
+++ src/components/SessionProvider.tsx
@@ -3,9 +3,5 @@
 import { SessionProvider as NextAuthSessionProvider } from 'next-auth/react';
 
 export default function SessionProvider({ children }: { children: React.ReactNode }) {
-  return (
-    <NextAuthSessionProvider refetchInterval={5 * 60} refetchOnWindowFocus={true}>
-      {children}
-    </NextAuthSessionProvider>
-  );
+  return <NextAuthSessionProvider>{children}</NextAuthSessionProvider>;
 } 
