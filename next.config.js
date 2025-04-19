3ad22065d016d5eac7bc260ba141055d8aaf3b52
Bach Minh Quang <0947556012abc@gmail.com>
2025-04-19 12:38:51 +0700

    fix: đơn giản hóa trang sản phẩm và giảm thiểu độ phức tạp render

--- next.config.js
+++ next.config.js
@@ -66,30 +66,8 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
   },
-  webpack: (config, { dev, isServer }) => {
-    config.optimization = {
-      ...config.optimization,
-      minimize: false,
-      minimizer: [],
-      splitChunks: false,
-      runtimeChunk: false,
-      flagIncludedChunks: false,
-      concatenateModules: false,
-      usedExports: false,
-      sideEffects: false,
-      providedExports: false,
-      innerGraph: false,
-      mangleExports: false,
-    };
-    
-    if (dev) {
-      config.mode = 'none';
-    }
-    
-    if (!isServer) {
-      config.output.libraryTarget = 'var';
-    }
-    
+  // Đơn giản hóa cấu hình webpack
+  webpack: (config) => {
     return config;
   },
   compiler: {
