63247ec55425307337139ef0d49cf8963f356b51
Bach Minh Quang <0947556012abc@gmail.com>
2025-05-18 02:29:08 +0700

    ui: move sales count below star rating

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -244,17 +244,8 @@ export default function ProductCard({
         </p>
         
         {/* Phần đánh giá */}
-        <div className="flex items-center justify-between mb-3">
-          <div className="flex items-center">
-            {weeklyPurchases > 0 && (
-              <div className="text-xs text-gray-500 flex items-center">
-                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-                  <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
-                </svg>
-                Đã bán: {weeklyPurchases}
-              </div>
-            )}
-          </div>
+        <div className="flex items-center justify-between mb-1">
+          <div></div>
           <div className="flex items-center">
             {rating > 0 && (
               <div className="flex items-center">
@@ -264,6 +255,18 @@ export default function ProductCard({
             )}
           </div>
         </div>
+        
+        {/* Phần đã bán */}
+        {weeklyPurchases > 0 && (
+          <div className="flex justify-end mb-3">
+            <div className="text-xs text-gray-500 flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+              </svg>
+              Đã bán: {weeklyPurchases}
+            </div>
+          </div>
+        )}
 
         {/* Phần giá */}
         <div className="mt-auto">
