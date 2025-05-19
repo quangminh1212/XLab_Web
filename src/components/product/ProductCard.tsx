60a48c5e583dd989c2d0cbe5ab2b85b83f557c39
Bach Minh Quang <0947556012abc@gmail.com>
2025-05-19 14:42:55 +0700

    Revert "Remove product rating display"
    This reverts commit 4177f0235d9109168642850ab105e87e69b4996a.

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -243,12 +243,22 @@ export default function ProductCard({
           {shortDescription}
         </p>
         
-        {weeklyPurchases > 0 && (
-          <div className="absolute bottom-1 right-1 text-xs text-gray-500 flex items-center">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
-              <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
-            </svg>
-            Đã bán: {weeklyPurchases}
+        {(rating > 0 || weeklyPurchases > 0) && (
+          <div className="absolute bottom-1 right-1 flex flex-col items-end gap-1">
+            {rating > 0 && (
+              <div className="flex items-center">
+                <span className="bg-yellow-50 text-xs font-medium text-yellow-600 px-1.5 py-0.5 rounded mr-1">{formatRating(rating)}</span>
+                {renderRatingStars(rating)}
+              </div>
+            )}
+            {weeklyPurchases > 0 && (
+              <div className="text-xs text-gray-500 flex items-center">
+                <svg xmlns="http://www.w3.org/2000/svg" className="h-3 w-3 mr-1 text-gray-400" viewBox="0 0 20 20" fill="currentColor">
+                  <path fillRule="evenodd" d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z" clipRule="evenodd" />
+                </svg>
+                Đã bán: {weeklyPurchases}
+              </div>
+            )}
           </div>
         )}
         
