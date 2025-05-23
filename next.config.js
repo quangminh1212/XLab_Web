4669548e54654ed2b4f88ec25e42fd57b17f90c2
Bach Minh Quang <0947556012abc@gmail.com>
2025-05-23 19:26:32 +0700

    refactor: remove suppressHydrationWarning from Next.js config

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const path = require('path');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
-    suppressHydrationWarning: true,
     optimizePackageImports: ['react-icons'],
   },
   reactStrictMode: true,
