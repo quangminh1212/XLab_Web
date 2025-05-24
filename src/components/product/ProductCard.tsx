ea85b0ce82cbfc241680fb4954d65f493b05816a
Bach Minh Quang <0947556012abc@gmail.com>
2025-05-24 15:00:44 +0700

    Further reduce short description font size by 30%

--- src/components/product/ProductCard.tsx
+++ src/components/product/ProductCard.tsx
@@ -269,7 +269,7 @@ const ProductCard = memo(function ProductCard({
         </h3>
         {shortDescription && (
           <div
-            className="text-[0.525rem] font-light text-gray-500 line-clamp-2 mb-2"
+            className="text-[0.37rem] font-light text-gray-500 line-clamp-2 mb-2"
             dangerouslySetInnerHTML={{ __html: shortDescription }}
           />
         )}
