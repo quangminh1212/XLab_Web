ec6225a60f30c88644704fbeca65092d94de131c
Bach Minh Quang <0947556012abc@gmail.com>
2025-06-14 02:26:39 +0700

    refactor: separate language files into individual modules

---
+++ src/locales/index.ts
@@ -0,0 +1,13 @@
+import vi from './vi';
+import en from './en';
+import es from './es';
+
+export type Language = 'vi' | 'en' | 'es';
+
+export const translations = {
+  vi,
+  en,
+  es
+};
+
+export default translations; 
