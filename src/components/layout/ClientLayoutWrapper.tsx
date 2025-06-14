f1533ba53af544cd87bb95a0c4d132048b3af555
Bach Minh Quang <0947556012abc@gmail.com>
2025-06-14 10:56:54 +0700

    Revert "feat: add auto language switcher to Spanish"
    This reverts commit 55cf007b5adaf4405aa706c93416d4ede6044f9f.

--- src/components/layout/ClientLayoutWrapper.tsx
+++ src/components/layout/ClientLayoutWrapper.tsx
@@ -8,7 +8,6 @@ import {
   StyleLoader,
   CssErrorHandler,
   GlobalStyles,
-  LanguageAutoSwitcher,
 } from '@/components/common';
 import { SessionProvider } from '@/components/auth';
 import { CartProvider } from '@/components/cart';
@@ -27,7 +26,6 @@ export default function ClientLayoutWrapper({ children }: ClientLayoutWrapperPro
         <NotificationProvider>
           <BalanceProvider>
             <CartProvider>
-              <LanguageAutoSwitcher />
               <div className="flex flex-col min-h-screen">
                 <Header />
                 <main className="flex-grow">{children}</main>
