ea441508fb8b66a79e04028d23a39c854687e8ae
Bach Minh Quang <0947556012abc@gmail.com>
2025-06-16 10:57:29 +0700

    fix: add missing SVG dropdown icon to LanguageSwitcher

--- src/components/common/LanguageSwitcher.tsx
+++ src/components/common/LanguageSwitcher.tsx
@@ -50,6 +50,20 @@ const LanguageSwitcher = ({ className = '' }: LanguageSwitcherProps) => {
           />
         </div>
         <span>{isVi ? 'VIE' : 'ENG'}</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-4 w-4 ml-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth={2}
+            d="M19 9l-7 7-7-7"
+          />
+        </svg>
       </button>
 
       {isOpen && (
